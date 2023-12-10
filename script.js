ccdade469aa12f5362077f585e040a7d6ab5a004
Brandon-loor <150089676+Brandon-loor@users.noreply.github.com>
2023-12-10 16:11:47 +0000

    Completed Project

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ function greetTheUser() {
   let nameUser = document.getElementById("nameUser").value
   let amountUser = document.getElementById("amountUser").value
   let emailUser = document.getElementById("emailUser").value
+  let creditUser = document.getElementById("creditUser").value
   let donationSpot = document.getElementById("greeting")
 
-  donationSpot.innerText = "Thank you for your donation, " + nameUser + "! We will reach out to " + emailUser + " to confirm your donation of $" + amountUser + "!"
+  if (amountUser > 0) {
+    donationSpot.innerText = "Thank you for your donation, " + nameUser + "! We will reach out to " + emailUser + " to confirm your donation of $" + amountUser + "!"
+  } 
+  else {
+    donationSpot.innerText = "Please enter a valid amount."
+  }
+  if (nameUser == "") {
+    donationSpot.innerText = "Please enter your name."
+  }
+  if (emailUser == "") {
+    donationSpot.innerText = "Please enter your email."
+  }
+  if (creditUser == "") {
+    donationSpot.innerText = "Please enter your credit card number."
+  }
 }
